d0678216b9824c836db2064aeb76c877c19bc8ef
tojo554 <mca554@rajagiri.edu>
2021-03-17 11:56:41 +0530

    student

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+      <div class="row">
+      <div class="col col-12 col-sm-6 col-md-4 col-lg-6 col-xl-12 col-xxl-12">
+    <table class="table table-borderless">
+        
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Course</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>roll no</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Admission No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>college</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+   
+    <tr>
+        <td></td>
+        <td><button class="btn btn-success">Submit </button></td>
+    </tr>
+    </div>
+    </div>
+    </div>
+
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
+
