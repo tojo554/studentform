fee168ee3d9be7c0bc630031614ab8c8ddae30a7
tojo554 <mca554@rajagiri.edu>
2021-03-17 12:49:25 +0530

    student

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="student-app\resources\views\welcome.blade.php">home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/contact">contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/faculty">Faculty</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 <body>
 <div class="container">
       <div class="row">
